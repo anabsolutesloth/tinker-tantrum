74186ff884bd302cc0d589cee1b08e2c849d2910
anabsolutesloth <63694982+anabsolutesloth@users.noreply.github.com>
2025-01-30 00:12:52 -0800

    Boots to game with dependencies :) make code tomorrow

--- src/main/java/com/emperdog/tinkertantrum/TinkerTantrumMod.java
+++ src/main/java/com/emperdog/tinkertantrum/TinkerTantrumMod.java
@@ -1,13 +1,13 @@
-package com.example.modid;
+package com.emperdog.tinkertantrum;
 
-import com.example.modid.Tags;
+import com.emperdog.tinkertantrum.Tags;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 @Mod(modid = Tags.MOD_ID, name = Tags.MOD_NAME, version = Tags.VERSION)
-public class ExampleMod {
+public class TinkerTantrumMod {
 
     public static final Logger LOGGER = LogManager.getLogger(Tags.MOD_NAME);
 
